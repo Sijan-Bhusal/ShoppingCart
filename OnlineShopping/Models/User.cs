02a17bafcf71488de47a7086087b183e00e99ca2
Sijan-Bhusal <sijan.bhusal03@gmail.com>
2025-08-05 16:13:54 +0545

    Added User Auth Features

---
+++ OnlineShopping/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [DataType(DataType.EmailAddress,ErrorMessage ="Invalid Email Address!")]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [StringLength(50,MinimumLength =6,ErrorMessage ="Password length should be greater than 6")]
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
